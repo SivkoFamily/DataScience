942a7353df6dc76b26ced414de939b9f0b25423c
sivko-evgene <104692425+sivko-evgene@users.noreply.github.com>
2023-01-23 22:28:54 +0300

    to new dir

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Welcome into our small and humble organization.
 
 Main questions to answer
 - What can we do together here? How can I help you?)
+
+My questions:
+1. I dont can install dotenv in my environment variables and can use my file .env. I can not understand what this libary dont work)
+this library install in my environment variables i cheked this. Import say 'ModuleNotFoundError: No module named 'dotenv''
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import json
 import berserk
-API_TOKEN = 'lip_ioxRbYOfrqq0PRgCBxOg'
-session = berserk.TokenSession(API_TOKEN)
-client = berserk.Client(session=session)
-data = client.users.get_leaderboard("bullet", count=5)
-print(data)
-type(data)
-with open('data.json', 'w') as json_file:
-    json.dump(data, json_file)
-    
+from config import API_TOKEN
+
+def get_data_lichess():
+    session = berserk.TokenSession(API_TOKEN)
+    client = berserk.Client(session=session)
+    data_bulle = client.users.get_leaderboard("bullet", count=200)
+
+result = get_data_lichess()
+print(result)
+with open('result.json', 'w') as json_file:
+    json.dump(result, json_file)
