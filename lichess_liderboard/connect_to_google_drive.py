e16bf13976f392dfdf7cfc8be52e092d0b07335f
sivko-evgene <104692425+sivko-evgene@users.noreply.github.com>
2023-05-25 00:18:09 +0300

    complete function upload_file_in_google_drive add save folder

--- lichess_liderboard/connect_to_google_drive.py
+++ lichess_liderboard/connect_to_google_drive.py
@@ -12,12 +12,17 @@ logging.basicConfig(
 def upload_file_in_google_drive(dir_path=''):
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
     logging.info('Start function upload_dir')
     try:
-        drive = GoogleDrive(gauth)
+        fileList = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+        for file in fileList:
+            print('Title: %s, ID: %s' % (file['title'], file['id']))
+            if(file['title'] == "data_for_tableau"):
+                fileID = file['id']
         for file_name in os.listdir(dir_path):
             print(file_name)
-            my_file = drive.CreateFile({'title': f'{file_name}'})
+            my_file = drive.CreateFile({'title': f'{file_name}', "mimeType": "text/csv", "parents": [{"kind": "drive#fileLink", "id": fileID}]})
             my_file.SetContentFile(os.path.join(dir_path, file_name))
             my_file.Upload()
         logging.info('Finish function upload_dir')
